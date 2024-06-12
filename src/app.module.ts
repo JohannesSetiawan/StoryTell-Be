274298c051350fb93bc3feceb983332968852335
yohaneswijaya12 <johannes.setiawan@ui.ac.id>
2024-06-12 09:10:40 +0700

    Module initialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { StoryModule } from './story/story.module';
+import { ChapterModule } from './chapter/chapter.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, StoryModule, ChapterModule],
   controllers: [AppController],
   providers: [AppService],
 })
