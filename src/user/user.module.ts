08ac5f5660c7cdb6776a54ca70f2e744dcefe159
yohaneswijaya12 <johannes.setiawan@ui.ac.id>
2024-06-12 12:20:56 +0700

    Fix prisma module import.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
