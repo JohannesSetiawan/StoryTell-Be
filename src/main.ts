43281966a455cc5d36b1ed1e352927fbf0fbb8a4
yohaneswijaya12 <johannes.setiawan@ui.ac.id>
2024-06-14 09:49:14 +0700

    Fix CORS error (2).

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
+      'http://localhost:5173',
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
