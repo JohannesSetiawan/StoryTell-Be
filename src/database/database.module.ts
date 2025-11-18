f0b4768d1acef36ee395deef6a808d93f04678c8
yohaneswijaya12 <johannes.setiawan@ui.ac.id>
2025-11-18 09:56:26 +0700

    [fix] DB config

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,9 +7,25 @@ const dbProvider = {
   provide: 'DATABASE_POOL',
   inject: [ConfigService],
   useFactory: (configService: ConfigService) => {
-    return new Pool({
+    const pool = new Pool({
       connectionString: configService.get<string>('DATABASE_URL'),
+      max: 20, // Maximum pool size
+      min: 2, // Minimum pool size
+      idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
+      connectionTimeoutMillis: 10000, // Return an error after 10 seconds if connection cannot be established
+      maxUses: 7500, // Close and replace a connection after it has been used 7500 times
+      statement_timeout: 30000, // Terminate any statement that takes more than 30 seconds
+      query_timeout: 30000, // Query timeout (30 seconds)
+      keepAlive: true, // Enable TCP keep-alive
+      keepAliveInitialDelayMillis: 10000, // Start keep-alive after 10 seconds
     });
+
+    // Handle pool errors
+    pool.on('error', (err, client) => {
+      console.error('Unexpected error on idle client', err);
+    });
+
+    return pool;
   },
 };
 
